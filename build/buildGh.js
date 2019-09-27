3c6ee3c42493f3709cb1cbc4fb0851fc18c43d72
luzhongkuan <luzhongk@126.com>
2019-09-27 18:11:26 +0800

    test

--- build/buildGh.js
+++ build/buildGh.js
@@ -1,9 +1,6 @@
 const fs = require('fs')
 console.log(__dirname)
-console.log(
-  fs.readdirSync(`${__dirname}/..`),
-  fs.readdirSync(`${__dirname}/../node_modules`)
-)
+console.log(fs.readdirSync(`${__dirname}/../../`))
 
 const { build } = require('../node_modules/kuan-pack/index.js')
 
